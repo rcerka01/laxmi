c9de1cf34085177cc24c4116cb63fe2d3a709a51
Raitis <raitis.cerkasovs@gmail.com>
2017-12-11 01:27:59 +0000

    add enviroment switch

--- mr-green/config/config.js
+++ mr-green/config/config.js
@@ -1,10 +1,42 @@
 var secret = require("./secret");
 
-module.exports = {
+var test = {
     app: {
+        live: false, 
         port: 3002,
         updateTimes: {
-            //seconds: [3, 13, 23, 33, 43, 53]
+            seconds: [3, 13, 23, 33, 43, 53]
+        }
+    },
+    mrGold: {
+        host: "raitis.co.uk",
+        protocol: "http",
+        port: 3001
+    },
+    vishnu: {
+        host: "raitis.co.uk",
+        protocol: "http",
+        port: 3003
+    },
+    db: {
+        domain: "@ds123796.mlab.com:23796/",
+        name: "betfairtestdb",
+        login: secret.testDb.login,
+        password: secret.testDb.password
+    },
+    soccer: {
+        bid: 5,
+        elapsedTime: 80,
+        scoreHigher: 2,
+        version: "test"
+    }
+}
+
+var live = {
+    app: {
+        live: true, 
+        port: 3002,
+        updateTimes: {
             seconds: [5]
         }
     },
@@ -24,13 +56,6 @@ module.exports = {
         login: secret.db.login,
         password: secret.db.password
     },
-    // [TEST DB]
-    // db: {
-    //     domain: "@ds123796.mlab.com:23796/",
-    //     name: "betfairtestdb",
-    //     login: secret.testDb.login,
-    //     password: secret.testDb.password
-    // },
     soccer: {
         bid: 5,
         elapsedTime: 80,
@@ -38,3 +63,5 @@ module.exports = {
         version: "v1.4"
     }
 }
+
+module.exports = live;
