31c6e2aeca6690d93e3c5ea9a0404647ee814d01
Ray <raitis.cerkasovs@gmail.com>
2020-07-10 00:55:36 +0100

    fixListMarkets api in Gold

--- mr-green/controllers/soccerMainController.js
+++ mr-green/controllers/soccerMainController.js
@@ -2,7 +2,7 @@ var schedule = require('node-schedule');
 var unirest = require('unirest');
 var conf = require("../config/config");
 var soccerEvents = require("./soccerEventController");
-var mainLoggs = require("./mainLoggController");
+var mainLoggs = require("./soccerLoggController");
 var soccerOdds = require("./soccerOddsController");
 
 const domainMrGold = conf.mrGold.protocol + "://" + conf.mrGold.host + ":" + conf.mrGold.port
@@ -12,7 +12,6 @@ var rule = new schedule.RecurrenceRule();
 rule.second = conf.app.updateTimes.seconds;
 
 var times = 0;
-var version = 0;
 
 const log = conf.app.log;
   
@@ -24,12 +23,13 @@ function updateSoccerGames(times) {
     // logging changes in account
     mainLoggs.loggAccountStatus(domainMrGold, times);
 
-    // IN PLAY GAMES. STRATEGIES BASED ON GAME STATUS
+
+    // 1. IN PLAY GAMES. STRATEGIES BASED ON GAME STATUS
     unirest.get(domainMrGold + '/api/listInPlaySoccerEvents')
         .end(function (gamesInPlayResponse) {
 
             // log
-            if (log) { console.log("Iteration: " + times +". Main controller. Got IN-PLAY soccer events from MR GOLD api"); }
+            if (log) { console.log("Iteration: " + times +". Main controller. Got IN-PLAY soccer events for EVENTS controller from MR GOLD api"); }
 
             //##### Pass in-play soccer events to soccer events controller
             soccerEvents.catchSoccerEvents(gamesInPlayResponse, domainVishnu, domainMrGold, times);
@@ -39,32 +39,38 @@ function updateSoccerGames(times) {
             console.log("Error rerieving listInPlaySoccerEvents from Mr Grold API: " + e.message);
     });
 
-    // ALL AND IN-PLAY GAMES. STRATEGIES BASED ON ODDS
+
+    // 2. ALL (before "in-play") GAMES. STRATEGIES BASED ON ODDS
     unirest.get(domainMrGold + '/api/listSoccerEvents')
         .end(function (gamesAllResponse) {
 
             // log
             if (log) { console.log("Iteration: " + times +". Main controller. Got ALL soccer events for ODDS controller from MR GOLD api"); }
 
-            unirest.get(domainMrGold + '/api/listInPlaySoccerEvents')
-            .end(function (gamesInPlayResponse) {
-    
-                // log
-                if (log) { console.log("Iteration: " + times +". Main controller. Got IN-PLAY soccer events for ODDS controller from MR GOLD api"); }
-    
-                //##### Pass in-play and all soccer events to soccer odds controller
-                soccerOdds.updateOddChanges(gamesAllResponse.body, gamesInPlayResponse.body, domainVishnu, domainMrGold, times);
-                //#####
+            //##### Pass all soccer events to soccer odds controller
+            soccerOdds.updateOddChanges(gamesAllResponse.body, domainMrGold, times, false);
+            //#####
 
-            })
-            .on('error', function(e) {
-                console.log("Error rerieving listInPlaySoccerEvents for ODDS controller from Mr Grold API: " + e.message);
-        });
+           })
+           .on('error', function(e) {
+                console.log("Error rerieving listSoccerEvents for ODDS controller from Mr Grold API: " + e.message);
+    });
 
-    })
-    .on('error', function(e) {
-        console.log("Error rerieving listSoccerEvents for ODDS controller from Mr Grold API: " + e.message);
-});
+
+    // 3. ALL IN-PLAY GAMES. STRATEGIES BASED ON ODDS
+    unirest.get(domainMrGold + '/api/listInPlaySoccerEvents')
+        .end(function (gamesInPlayResponse) {
+
+            // // log
+            // if (log) { console.log("Iteration: " + times +". Main controller. Got IN-PLAY soccer events for ODDS controller from MR GOLD api"); }
+    
+            // //##### Pass in-play soccer events to soccer odds controller
+            // soccerOdds.updateOddChanges(gamesInPlayResponse.body, domainMrGold, times, true);
+            // //#####
+        })
+        .on('error', function(e) {
+            console.log("Error rerieving listInPlaySoccerEvents for ODDS controller from Mr Grold API: " + e.message);
+    });
 }
 
 module.exports = { run: function(app) { 
--- mr-green/controllers/soccerOddsController.js
+++ mr-green/controllers/soccerOddsController.js
@@ -3,7 +3,7 @@ var conf = require("../config/config");
 
 const log = conf.app.log;
 
-function updateOddChanges(games, gamesInPlay, domainVishnu, domainMrGold, times) {
+function updateOddChanges(games, domainMrGold, times, isInline) {
 
     // retrieve all event Ids passed from main controller
     var eventIds = "";
@@ -27,14 +27,80 @@ function updateOddChanges(games, gamesInPlay, domainVishnu, domainMrGold, times)
             
                 var marketCatalogue = marketCatalogueResponse.body
 
-                // retrieve all market Ids
-                var marketIds = "";
-                for(var i in marketCatalogue) {
-                    marketIds = marketIds + marketCatalogue[i].item.marketId + ",";
+                function chunk(arr, chunkSize) {
+                    var R = [];
+                    for (var i=0,len=arr.length; i<len; i+=chunkSize)
+                      R.push(arr.slice(i,i+chunkSize));
+                    return R;
                 }
 
-                //console.log(marketIds);
-            
+                var  marketCatalogueDiv = chunk(marketCatalogue, 20);
+
+                for (var i in marketCatalogueDiv) {
+
+                    //retrieve all market Ids in subset
+                    var marketIds = "";
+                    var eventIds = "";
+                    var eventName = "";
+                    var eventCountrycode = "";
+                    var competition = "";
+
+                    for (var ii in marketCatalogueDiv[i]) {
+                        marketIds = marketIds + marketCatalogueDiv[i][ii].item.marketId + ",";
+                        eventIds = eventIds + marketCatalogueDiv[i][ii].item.event.id + ",";
+                        eventName = eventName + marketCatalogueDiv[i][ii].item.event.name + ",";
+                        eventCountrycode = eventCountrycode + marketCatalogueDiv[i][ii].item.event.countryCode + ",";
+                        competition = competition + marketCatalogueDiv[i][ii].item.competition.name + ",";
+                    }
+
+                    // console.log("XXXX " + eventIds)
+                    // console.log("YYYY " + eventName)
+                    // console.log("ZZZZ " + eventCountrycode)
+                    // console.log("NNNN " + competition)
+
+                    // RETRIEVE ODDS
+                    unirest.get(domainMrGold + '/api/listMarketBet/' + marketIds + "/" + eventIds + "/" + eventName + "/" + eventCountrycode + "/" + competition)
+                           .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+                           .end(function (marketResponses) {
+
+                                var markets = marketResponses.body;
+
+                                // var odds = [];
+                                // for (var mr in markets) {
+
+                                //     odds.push({
+                                //         marketId: markets[mr].item.marketId,
+                                //        // eventId: ids[mr],
+                                //         date: new Date(),
+                                //         totalMatched: markets[mr].item.totalMatched,
+                                //         selection: [{
+                                //             selectionId:        markets[mr].item.runners[0].selectionId,
+                                //             totalRunnerMatched: markets[mr].item.runners[0].totalMatched,
+                                //             back: markets[mr].item.runners[0].ex.availableToBack[0],
+                                //             lay: markets[mr].item.runners[0].ex.availableToLay[0] },
+                                //            {
+                                //             selectionId:        markets[mr].item.runners[1].selectionId,
+                                //             totalRunnerMatched: markets[mr].item.runners[1].totalMatched,
+                                //             back: markets[mr].item.runners[1].ex.availableToBack[0],
+                                //             lay: markets[mr].item.runners[1].ex.availableToLay[0] },
+                                //            {
+                                //             date: new Date(),
+                                //             selectionId:        markets[mr].item.runners[2].selectionId,
+                                //             totalRunnerMatched: markets[mr].item.runners[2].totalMatched,
+                                //             back: markets[mr].item.runners[2].ex.availableToBack[0],
+                                //             lay: markets[mr].item.runners[2].ex.availableToLay[0]
+                                //         }]   
+                                //     });
+
+                                // }
+
+                                // console.log();
+                                // console.log(odds.length + " #############################################################");
+                                // console.log(odds);
+                                // console.log("#############################################################");
+                           });
+
+                }
             }
         });
 
